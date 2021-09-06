8a385e1ecc3ab87dd6bb7bb9098cea4f6f14c3c9
Takafumi Arakaki <aka.tkf@gmail.com>
2021-09-05 21:03:25 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/Restarts.jl
@@ -0,0 +1,11 @@
+baremodule Restarts
+
+module Internal
+
+using ..Restarts: Restarts
+
+include("internal.jl")
+
+end  # module Internal
+
+end  # baremodule Restarts
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Restarts
+using Test
+
+@testset "Restarts.jl" begin
+    # Write your tests here.
+end
